5d2194965da8db99fe9f964d88d9f0276bbc8756
Sibabrat Swain <sibabrat.swain@nettantra.net>
2018-10-27 14:56:17 +0530

    modified attendees contract

--- contracts/Attendees.sol
+++ contracts/Attendees.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.24;
-
 contract Attendees {
-
-    // instation of structure
+    // instantiation  of structure
     struct Attendees {
         uint uid;
         address public_key;
@@ -10,7 +8,7 @@ contract Attendees {
         string img_url;
     }
 
-    //mapping of strAZucture for storing the participants
+    //mapping of structure  for storing the participants
     mapping( uint => Attendees) public attendees;
     uint public attendeesCount;
 
@@ -18,6 +16,7 @@ contract Attendees {
     constructor() public {
         addAttendee("Biswaindu","https://amp.businessinsider.com/images/5ac518b57a74af23008b4642-750-563.jpg");
         addAttendee("Sibabrat","https://www.evolllution.com/wp-content/uploads/2015/03/sized_Big-Name-Universities-Must-Respond-to-Student-Expectations.jpg");
+        addAttendee("Smruti","https://qph.fs.quoracdn.net/main-thumb-346046018-200-bybjsssbfqgxvocvdtdggibtjeejnmsk.jpeg");
     }
 
     // add a new participant to Participant structure
