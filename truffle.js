37d7847430c927fd8f0b1f456ecde8f27419b47c
Sibabrat Swain <sibabrat.swain@nettantra.net>
2018-11-02 22:56:49 +0530

    Adding hd wallet and praparing for

--- truffle.js
+++ truffle.js
@@ -1,11 +1,36 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    `http://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+                )
+            },
+            gasPrice: 2500000000,
+            network_id: "3",
+            port: 8545
+        },
+        rinkeby:  {
+            network_id: 4,
+            host: "localhost",
+            port:  8545,
+            gas:   4700000
+        },
+        solc: {
+            optimizer: {
+                enable: true,
+                runs: 200
+            }
+        }
     }
-  }
 };
+
