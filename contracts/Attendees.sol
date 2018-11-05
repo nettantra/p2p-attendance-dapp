befaac43697eba422239087070b72f97bf47278e
Sibabrat Swain <sibabrat.swain@nettantra.net>
2018-11-05 23:26:08 +0530

    worked on user authentication

--- contracts/Attendees.sol
+++ contracts/Attendees.sol
@@ -20,10 +20,10 @@ contract Attendees {
     // constructor to save some attendees
     constructor() public {
         owner = msg.sender;
-        addAttendee("Biswaindu", "https://amp.businessinsider.com/images/5ac518b57a74af23008b4642-750-563.jpg", 0x3b220bdD0D1C1b37AC6d434f027CC88a5b51B878);
-        addAttendee("Sibabrat", "https://www.evolllution.com/wp-content/uploads/2015/03/sized_Big-Name-Universities-Must-Respond-to-Student-Expectations.jpg", 0x0c8615A3d73b0AA9342A06A3d66C5f723D63E2Ed);
-        addAttendee("Smruti", "https://m.blog.hu/do/dolgozzmagadon/image/student2.jpg", 0x9D5AeBBaf8026021ad33A27748ae7d5E94C7E891);
-        addAttendee("Prabina", "https://www.ldatschool.ca/wp-content/uploads/2014/02/A-teachers-journey-with-student-self-advocacy.jpg", 0xab70787066CDeEb7f402b7364fac3a8BD7851c8D);
+        addAttendee("Biswaindu", "https://amp.businessinsider.com/images/5ac518b57a74af23008b4642-750-563.jpg", 0x848f3DB5C37F8a7aDdEf99704C43e544c8E18207);
+        addAttendee("Sibabrat", "https://www.evolllution.com/wp-content/uploads/2015/03/sized_Big-Name-Universities-Must-Respond-to-Student-Expectations.jpg", 0xA81cD3109F53Fb5604C2c2b3f3D8debf8a1dDc49);
+        addAttendee("Smruti", "https://m.blog.hu/do/dolgozzmagadon/image/student2.jpg", 0xD1bAaAA14639C468f47C89Eb9028b6d6A1007Da4);
+        addAttendee("Prabina", "https://www.ldatschool.ca/wp-content/uploads/2014/02/A-teachers-journey-with-student-self-advocacy.jpg", 0x881274dBaD173E92586621fde2b2325B63ea7617);
     }
 
     // modifier to add the attendee by owner only
@@ -37,6 +37,17 @@ contract Attendees {
         attendeesCount++;
         attendees[attendeesCount] = AttendeesStructure(attendeesCount, public_key, name, img_url);
     }
+
+
+    // authenticate users
+    function authenticateUser(address _userAdd) public view returns (bool) {
+        for(uint i = 1; i<=attendeesCount; i++){
+            if(attendees[i].public_key == _userAdd) return  true ;
+        }
+        return false;
+    }
+
+
 }
 
 contract MarkAttendance is Attendees {
@@ -65,17 +76,18 @@ contract MarkAttendance is Attendees {
         attendeeDetails[attendeeDetailsCount] = AttendeeDetails(msg.sender, attendee, attendance_opinion, now, date);
     }
 
-    function getMarkedAttendeeDetailsCount() view public returns (uint) {return attendeeDetailsCount;}
+    // fetter function for attendee details count
+    function getMarkedAttendeeDetailsCount() public view returns (uint) {return attendeeDetailsCount;}
 
     //getter function for attendee details
-    function getAttendeeDetails(uint _count, uint256 date) view public returns (address, uint, uint256) {
+    function getAttendeeDetails(uint _count, uint256 date) public view returns (address, uint, uint256) {
         address attendee_add = attendees[_count].public_key;
         uint256 doa = attendeeDetails[_count].date_of_attendance;
         uint present = 0;
         uint absent = 0;
         uint opinion = 3;
         for (uint i = 0; i <= attendeeDetailsCount; i++) {
-            if (attendee_add == attendeeDetails[i].attendee)
+            if (attendee_add == attendeeDetails[i].attendee && doa == attendeeDetails[i].date_of_attendance)
             {
                 if (attendeeDetails[i].attendance_opinion == 1) present++;
                 else if (attendeeDetails[i].attendance_opinion == 2) absent++;
@@ -90,12 +102,6 @@ contract MarkAttendance is Attendees {
 
         return (attendee_add, opinion, date);
     }
-
-    // check if marked attendance already
-    function validateAttender() internal returns (bool){
-
-    }
-
 
 }
 
@@ -133,4 +139,5 @@ contract EvaluateAttendance is MarkAttendance {
             evaluateCount++;
         }
     }
+
 }
