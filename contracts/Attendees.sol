193d0952115319925f8148962c32a601efa31dbd
Sibabrat Swain <sibabrat.swain@nettantra.net>
2018-11-20 14:15:57 +0530

    modified contract

--- contracts/Attendees.sol
+++ contracts/Attendees.sol
@@ -7,6 +7,7 @@ contract Attendees {
         uint uid;
         address public_key;
         string name;
+        string about;
         string img_url;
     }
 
@@ -20,12 +21,12 @@ contract Attendees {
     // constructor to save some attendees
     constructor() public {
         owner = msg.sender;
-        addAttendee("Biswaindu", "https://amp.businessinsider.com/images/5ac518b57a74af23008b4642-750-563.jpg", 0x3b220bdD0D1C1b37AC6d434f027CC88a5b51B878);
-        addAttendee("Sibabrat", "https://www.evolllution.com/wp-content/uploads/2015/03/sized_Big-Name-Universities-Must-Respond-to-Student-Expectations.jpg", 0x0c8615A3d73b0AA9342A06A3d66C5f723D63E2Ed);
-        addAttendee("Smruti", "https://m.blog.hu/do/dolgozzmagadon/image/student2.jpg", 0x9D5AeBBaf8026021ad33A27748ae7d5E94C7E891);
-        addAttendee("Prabina", "https://www.ldatschool.ca/wp-content/uploads/2014/02/A-teachers-journey-with-student-self-advocacy.jpg", 0xab70787066CDeEb7f402b7364fac3a8BD7851c8D);
-        addAttendee("Dev Sir", "https://d1ay3zqomhlmi.cloudfront.net/wp-content/uploads/2018/07/about-image.jpg", 0x54a51E5d44C35F8C175270d98fd633440f929D21);
-        addAttendee("Pitabash Sir", "https://d1ay3zqomhlmi.cloudfront.net/wp-content/uploads/2018/07/nettantra-logo-white-large-466x140.png", 0xfb98737a67F6f577f9b88F5496e95CB98601C3E7);
+        addAttendee("Biswaindu", "Content Writer", "https://amp.businessinsider.com/images/5ac518b57a74af23008b4642-750-563.jpg", 0x3b220bdD0D1C1b37AC6d434f027CC88a5b51B878);
+        addAttendee("Sibabrat", "Application Developer", "https://www.evolllution.com/wp-content/uploads/2015/03/sized_Big-Name-Universities-Must-Respond-to-Student-Expectations.jpg", 0x0c8615A3d73b0AA9342A06A3d66C5f723D63E2Ed);
+        addAttendee("Smruti", "Content Writer", "https://m.blog.hu/do/dolgozzmagadon/image/student2.jpg", 0x9D5AeBBaf8026021ad33A27748ae7d5E94C7E891);
+        addAttendee("Prabina", "Developer Trainee", "https://www.ldatschool.ca/wp-content/uploads/2014/02/A-teachers-journey-with-student-self-advocacy.jpg", 0xab70787066CDeEb7f402b7364fac3a8BD7851c8D);
+        addAttendee("Dev Sir", "CTO/CEO", "https://d1ay3zqomhlmi.cloudfront.net/wp-content/uploads/2018/07/about-image.jpg", 0x54a51E5d44C35F8C175270d98fd633440f929D21);
+        addAttendee("Pitabash Sir", "Software Developer", "https://d1ay3zqomhlmi.cloudfront.net/wp-content/uploads/2018/07/development-section.png", 0xfb98737a67F6f577f9b88F5496e95CB98601C3E7);
     }
 
     // modifier to add the attendee by owner only
@@ -35,15 +36,15 @@ contract Attendees {
     }
 
     // add attendee to attendees mapping
-    function addAttendee(string name, string img_url, address public_key) onlyOwner public {
+    function addAttendee(string name, string about, string img_url, address public_key) onlyOwner public {
         attendeesCount++;
-        attendees[attendeesCount] = AttendeesStructure(attendeesCount, public_key, name, img_url);
+        attendees[attendeesCount] = AttendeesStructure(attendeesCount, public_key, name, about, img_url);
     }
 
     // authenticate users
     function authenticateUser(address _userAdd) public view returns (bool) {
-        for(uint i = 1; i<=attendeesCount; i++){
-            if(attendees[i].public_key == _userAdd) return  true ;
+        for (uint i = 1; i <= attendeesCount; i++) {
+            if (attendees[i].public_key == _userAdd) return true;
         }
         return false;
     }
@@ -63,7 +64,7 @@ contract MarkAttendance is Attendees {
 
     //mapping of structure  for storing the attendeeDetails
     mapping(uint => AttendeeDetails) public attendeeDetails;
-    uint public attendeeDetailsCount = 0;
+    uint public attendeeDetailsCount;
 
     //constructor
     constructor() public {
@@ -86,8 +87,8 @@ contract MarkAttendance is Attendees {
         uint present = 0;
         uint absent = 0;
         uint opinion = 3;
-        for (uint i = 0; i <= attendeeDetailsCount; i++) {
-            if (attendee_add == attendeeDetails[i].attendee && doa == attendeeDetails[i].date_of_attendance)
+        for (uint i = 1; i <= attendeeDetailsCount; i++) {
+            if (attendee_add == attendeeDetails[i].attendee && doa == date)
             {
                 if (attendeeDetails[i].attendance_opinion == 1) present++;
                 else if (attendeeDetails[i].attendance_opinion == 2) absent++;
@@ -117,7 +118,7 @@ contract EvaluateAttendance is MarkAttendance {
 
     //mapping of structure for storing the evaluated_attendees
     mapping(uint => EvaluatedAttendee) public evaluated_attendees;
-    uint public evaluateCount = 1;
+    uint public evaluateCount;
 
     uint public r_opinion = 3;
     address public r_attendee_address;
