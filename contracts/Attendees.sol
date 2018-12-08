95aefea171b716e0e2b3402712e330e500b948dc
Sibabrat Swain <sibabrat.swain@nettantra.net>
2018-12-08 19:10:19 +0530

    fixed alias issues with uint

--- contracts/Attendees.sol
+++ contracts/Attendees.sol
@@ -4,31 +4,32 @@ contract Attendees {
 
     // instantiation of structure
     struct AttendeesStructure {
-        uint uid;
+        uint256 uid;
         address public_key;
         string name;
         string about;
         string img_url;
+        uint256 status; // 1 = Active 2 = deleted
     }
 
     address owner;
 
     //mapping of structure for storing the attendees
-    mapping(uint => AttendeesStructure) public attendees;
-    uint public attendeesCount;
+    mapping(uint256 => AttendeesStructure) public attendees;
+    uint256 public attendeesCount;
 
     //1540944000
     // constructor to save some attendees
     constructor() public {
         owner = msg.sender;
-        addAttendee("Devadutta Sahoo", "CEO/CTO", "https://img.theweek.in/content/dam/week/news/entertainment/images/2018/3/13/amitabh-bachchan-salil-bera.jpg", 0xE4b5d53BDe0Ea9948cCe22cA6411F70b28E38f41);
-        addAttendee("Swetansu Mohapatra", "Application Developer", "http://stat2.bollywoodhungama.in/wp-content/uploads/2017/05/Sanjay-Dutt-walks-out-of-Total-Dhamaal.jpg", 0xaaaBe0cf13cd5A722561c293602851714D2e593d);
-        addAttendee("Pitabas Behera", "Managing Director", "https://s3.india.com/wp-content/uploads/2017/06/Aamir-Khan.jpg", 0x3590415CD6596Da5015CD2810e5F5Ac04E6D7A77);
-        addAttendee("Sanjeev Nanda", "Human Resources", "https://img.etimg.com/thumb/height-480,width-640,imgsize-9960,msid-56595593/.jpg", 0x79e32f46b4C1fC8EDE6465Bd17E2D64CE4a2A41b);
-        addAttendee("Prabina Parichha", "Application Trainee", "http://2.bp.blogspot.com/__5vnMiF3qLg/TO-0BpSXIhI/AAAAAAAAC-U/nHbC0PrtvHI/s1600/Genelia%252BDsouza%252Bcute.jpg", 0x1CC4ffc74a149b6aACA316e80235f7D62f161b20);
-        addAttendee("SmrutiRekha Panda", "Content Writer", "https://s3.india.com/wp-content/uploads/2017/08/ileana-d-cruz-6.png", 0x1AaED6720B2e7Cb215b8b22870153C53EACf4A85);
-        addAttendee("Sibabrat Swain", "Application Developer", "http://cdn.persiangig.com/preview/7tb5eZgr3W/large/6106.jpg", 0x669D42d970F7ab357fEe9615283ef34FaFAd9f14);
-        addAttendee("Biswaindu Parida", "Content Writer", "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2016/11/anil_kapoor_-_getty_-_h_-_2016.jpg", 0x97E290c59bc4EB459e4911c0e56ba8ef668F8FF9);
+        addAttendee("Devadutta Sahoo", "CEO/CTO", "https://img.theweek.in/content/dam/week/news/entertainment/images/2018/3/13/amitabh-bachchan-salil-bera.jpg", 0xE4b5d53BDe0Ea9948cCe22cA6411F70b28E38f41, 1);
+        addAttendee("Swetansu Mohapatra", "Application Developer", "http://stat2.bollywoodhungama.in/wp-content/uploads/2017/05/Sanjay-Dutt-walks-out-of-Total-Dhamaal.jpg", 0xaaaBe0cf13cd5A722561c293602851714D2e593d, 1);
+        addAttendee("Pitabas Behera", "Managing Director", "https://s3.india.com/wp-content/uploads/2017/06/Aamir-Khan.jpg", 0x3590415CD6596Da5015CD2810e5F5Ac04E6D7A77, 1);
+        addAttendee("Sanjeev Nanda", "Human Resources", "https://img.etimg.com/thumb/height-480,width-640,imgsize-9960,msid-56595593/.jpg", 0x79e32f46b4C1fC8EDE6465Bd17E2D64CE4a2A41b, 1);
+        addAttendee("Prabina Parichha", "Application Trainee", "http://2.bp.blogspot.com/__5vnMiF3qLg/TO-0BpSXIhI/AAAAAAAAC-U/nHbC0PrtvHI/s1600/Genelia%252BDsouza%252Bcute.jpg", 0x1CC4ffc74a149b6aACA316e80235f7D62f161b20, 1);
+        addAttendee("SmrutiRekha Panda", "Content Writer", "https://s3.india.com/wp-content/uploads/2017/08/ileana-d-cruz-6.png", 0x1AaED6720B2e7Cb215b8b22870153C53EACf4A85, 1);
+        addAttendee("Sibabrat Swain", "Application Developer", "http://cdn.persiangig.com/preview/7tb5eZgr3W/large/6106.jpg", 0x669D42d970F7ab357fEe9615283ef34FaFAd9f14, 1);
+        addAttendee("Biswaindu Parida", "Content Writer", "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2016/11/anil_kapoor_-_getty_-_h_-_2016.jpg", 0x97E290c59bc4EB459e4911c0e56ba8ef668F8FF9, 1);
     }
 
     // modifier to add the attendee by owner only
@@ -38,28 +39,38 @@ contract Attendees {
     }
 
     // add attendee to attendees mapping
-    function addAttendee(string _name, string _about, string _img_url, address _public_key) onlyOwner public {
+    function addAttendee(string _name, string _about, string _img_url, address _public_key, uint256 status) onlyOwner public {
         attendeesCount++;
-        attendees[attendeesCount] = AttendeesStructure(attendeesCount, _public_key, _name, _about, _img_url);
+        attendees[attendeesCount] = AttendeesStructure(attendeesCount, _public_key, _name, _about, _img_url, status);
     }
 
     // authenticate users
     function authenticateUser(address _user_add) public view returns (bool) {
-        for (uint i = 1; i <= attendeesCount; i++) {
-            if (attendees[i].public_key == _user_add) return true;
+        for (uint256 i = 1; i <= attendeesCount; i++) {
+            if (attendees[i].public_key == _user_add && attendees[i].status == 1) return true;
         }
         return false;
     }
 
     // for updating attendee
-    function updateAttendee(address _user_add, string _name, string _about, string _image) public {
-        for (uint i = 1; i <= attendeesCount; i++) {
+    function updateAttendee(address _user_add, string _name, string _about, string _image, uint256 status) public {
+        for (uint256 i = 1; i <= attendeesCount; i++) {
             if (attendees[i].public_key == _user_add) {
-                attendees[i] = AttendeesStructure(i, _user_add, _name, _about, _image);
+                attendees[i] = AttendeesStructure(i, _user_add, _name, _about, _image, status);
             }
         }
     }
 
+    function changeStatusEmployee(address _employeeAdd, uint256 status) onlyOwner public returns (bool) {
+        for (uint256 i = 1; i <= attendeesCount; i++) {
+            if (attendees[i].public_key == _employeeAdd) {
+                attendees[i].status = status;
+//                attendees[i].public_key = 0*0;
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
 
@@ -69,14 +80,14 @@ contract MarkAttendance is Attendees {
     struct AttendeeDetails {
         address attendance_giver;
         address attendee;
-        uint attendance_opinion;
+        uint256 attendance_opinion;
         uint256 timestamp;
         uint256 date_of_attendance;
     }
 
     //mapping of structure  for storing the attendeeDetails
-    mapping(uint => AttendeeDetails) public attendeeDetails;
-    uint public attendeeDetailsCount;
+    mapping(uint256 => AttendeeDetails) public attendeeDetails;
+    uint256 public attendeeDetailsCount;
 
     //constructor
     constructor() public {
@@ -84,22 +95,22 @@ contract MarkAttendance is Attendees {
     }
 
     // save mark attendance details to attendeeDetails mapping
-    function markAttendance(address _attendee, uint _attendance_opinion, uint256 _date) public {
+    function markAttendance(address _attendee, uint256 _attendance_opinion, uint256 _date) public {
         attendeeDetailsCount ++;
         attendeeDetails[attendeeDetailsCount] = AttendeeDetails(msg.sender, _attendee, _attendance_opinion, now, _date);
     }
 
     // fetter function for attendee details count
-    function getMarkedAttendeeDetailsCount() public view returns (uint) {return attendeeDetailsCount;}
+    function getMarkedAttendeeDetailsCount() public view returns (uint256) {return attendeeDetailsCount;}
 
     //getter function for attendee details
-    function getAttendeeDetails(uint _count, uint256 _date) public view returns (address, uint, uint256) {
+    function getAttendeeDetails(uint256 _count, uint256 _date) public view returns (address, uint256, uint256) {
         address attendee_add = attendees[_count].public_key;
         uint256 doa = attendeeDetails[_count].date_of_attendance;
-        uint present = 0;
-        uint absent = 0;
-        uint opinion = 3;
-        for (uint i = 1; i <= attendeeDetailsCount; i++) {
+        uint256 present = 0;
+        uint256 absent = 0;
+        uint256 opinion = 3;
+        for (uint256 i = 1; i <= attendeeDetailsCount; i++) {
             if (attendee_add == attendeeDetails[i].attendee && doa == _date)
             {
                 if (attendeeDetails[i].attendance_opinion == 1) present++;
@@ -118,7 +129,7 @@ contract MarkAttendance is Attendees {
 
     // validate marking attendance
     function validateAttendance(address _attendee, uint256 _date) public view returns (bool){
-        for (uint i = 1; i <= attendeeDetailsCount; i++) {
+        for (uint256 i = 1; i <= attendeeDetailsCount; i++) {
             if (attendeeDetails[i].attendance_giver == msg.sender && attendeeDetails[i].attendee == _attendee && attendeeDetails[i].date_of_attendance == _date) {
                 return true;
             }
@@ -132,16 +143,16 @@ contract EvaluateAttendance is MarkAttendance {
     // instantiation  of structure
     struct EvaluatedAttendee {
         address attendee_address;
-        uint opinion;
+        uint256 opinion;
         uint256 date_of_attendance;
         uint256 date_evaluated;
     }
 
     //mapping of structure for storing the evaluated_attendees
-    mapping(uint => EvaluatedAttendee) public evaluated_attendees;
-    uint public evaluateCount;
+    mapping(uint256 => EvaluatedAttendee) public evaluated_attendees;
+    uint256 public evaluateCount;
 
-    uint public r_opinion = 3;
+    uint256 public r_opinion = 3;
     address public r_attendee_address;
     uint256 public r_date_of_attendance;
 
@@ -155,7 +166,7 @@ contract EvaluateAttendance is MarkAttendance {
         r_opinion;
         r_date_of_attendance;
         r_attendee_address;
-        for (uint i = 1; i <= attendeesCount; i++) {
+        for (uint256 i = 1; i <= attendeesCount; i++) {
             (r_attendee_address, r_opinion, r_date_of_attendance) = getAttendeeDetails(i, _date);
             evaluated_attendees[evaluateCount] = EvaluatedAttendee(r_attendee_address, r_opinion, r_date_of_attendance, now);
             evaluateCount++;
@@ -163,11 +174,11 @@ contract EvaluateAttendance is MarkAttendance {
     }
 
     // for getting attendance result as per date and address
-    function attendanceResult(uint256 _date, address _addr) public view returns (uint) {
-        uint present = 0;
-        uint absent = 0;
-        uint opinion = 3;
-        for (uint i = 1; i <= attendeeDetailsCount; i++) {
+    function attendanceResult(uint256 _date, address _addr) public view returns (uint256) {
+        uint256 present = 0;
+        uint256 absent = 0;
+        uint256 opinion = 3;
+        for (uint256 i = 1; i <= attendeeDetailsCount; i++) {
             if (_addr == attendeeDetails[i].attendee && attendeeDetails[i].date_of_attendance == _date)
             {
                 if (attendeeDetails[i].attendance_opinion == 1) present++;
